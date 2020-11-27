99b32df35ec38c9dd4f3680cfdab756081d60cde
José Vicente <josevicentetejero@yahoo.es>
2020-11-27 20:56:59 +0100

    Add project files.

---
+++ CitiesWebsite/Services/ICityProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CitiesWebsite.Models;
+
+namespace CitiesWebsite.Services
+{
+    public interface ICityProvider : IEnumerable<KeyValuePair<string, City>>
+    {
+        City this[string name] { get; }
+    }
+}
+ 
